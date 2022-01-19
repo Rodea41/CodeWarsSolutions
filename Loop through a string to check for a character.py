480a331ab10a793960d71f999d64d14b1f098f18
Chris <ChrisRodea@gmail.com>
2022-01-19 15:19:20 -0800

    Added string.strip to code

--- Loop through a string to check for a character.py
+++ Loop through a string to check for a character.py
@@ -1,18 +1,24 @@
 def valid_parentheses(string):          ### Create the function
     str1 = str(string) ### If the parameter 'string' contains numbers and letters you must convert it all to a string
+
     open1 = 0  # Create an empty variable to count how many open parantheses
+
     close = 0  # Create empty var to count closed parantheses
-    
+
+    strip_spaces = string.strip()           ## Strips the string of any leading spaces. For example "   ("
     
     if str1[0] == ')':                      ## Checks if string starts with a 'close' parantheses . Auto reject if true
             return print('false')
-    else:                                   ## Goes through each letter to check for '(' and adds to var open1
-        for letters in range(len(string)):  ## IMPORTANT ## YOU MUST USE 'range(len(string)' in your loop statement or you will get an error saying 
-            if string[letters] == '(':      ## TypeError: string indices must be integers
+
+    else:                                        ## Goes through each letter to check for '(' and adds to var open1
+        for letters in range(len(strip_spaces)):  ## IMPORTANT ## YOU MUST USE 'range(len(strip_spaces))' in your loop statement or you will get an error saying 
+            if strip_spaces[letters] == '(':          ## 'TypeError: string indices must be integers' 
                 open1 += 1
+                continue
                 
-            elif string[letters] == ')':    # Goes through each letter to check for ')' and adds to var close
+            elif strip_spaces[letters] == ')':    # Goes through each letter to check for ')' and adds to var close
                 close += 1
+                continue
 
     if open1 == close:                      ## Returns true if there is a same amount of open and close parantheses. false if uneven amount. 
         return print("true")
@@ -20,4 +26,4 @@ def valid_parentheses(string):          ### Create the function
         return print("false")
 
 
-valid_parentheses("(())((()())())")
+valid_parentheses("   (())")
